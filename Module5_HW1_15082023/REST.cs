9205be056b7804b79dc643d971cedac0d7e1a8dd
Tymoshchuk Maksym <MTimoshchukmn.ua@gmail.com>
2023-08-16 14:55:30 +0300

    added methods GET and Create

---
+++ Module5_HW1_15082023/REST.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Module5_HW1_15082023
+{
+    internal class REST
+    {
+        private const string URL = @"https://reqres.in/api/";
+
+        public static async Task Get(string urlParameters)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                var res = await httpClient.GetAsync($"{URL}{urlParameters}");
+
+                if (res.StatusCode == HttpStatusCode.OK)
+                {
+                    var context = await res.Content.ReadAsStringAsync();
+                }
+            }
+        }
+
+        public static async Task Create(string urlParameters)
+        {
+            var jobParam = new
+            {
+                name = "morpheus",
+                job = "leader",
+            };
+
+            using (var httpClient = new HttpClient())
+            {
+                var context = new StringContent(
+                    JsonConvert.SerializeObject(jobParam),
+                    Encoding.UTF8,
+                    "application/json");
+
+                var res = await httpClient.PostAsync($"{URL}{urlParameters}", context);
+            }
+        }
+    }
+}
---
+++ Module5_HW1_15082023/Starter.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module5_HW1_15082023
+{
+    public class Starter
+    {
+        
+
+        public static void Run()
+        {
+            SendAsync().GetAwaiter().GetResult();
+            Console.ReadKey();
+        }
+
+        private static async Task SendAsync()
+        {
+            string urlParameters = string.Empty;
+
+            // GET LIST USERS
+            urlParameters = @"users?page=2";
+            await REST.Get(urlParameters);
+
+            // SINGLE USER
+            urlParameters = @"users/2";
+            await REST.Get(urlParameters);
+
+            // SINGLE USER NOT FOUND
+            urlParameters = @"users/23";
+            await REST.Get(urlParameters);
+
+            // LIST <RESOURCE>
+            urlParameters = @"unknown";
+            await REST.Get(urlParameters);
+
+            // SINGLE <RESOURCE>
+            urlParameters = @"unknown/2";
+            await REST.Get(urlParameters);
+
+            // SINGLE <RESOURCE> NOT FOUND
+            urlParameters = @"unknown/23";
+            await REST.Get(urlParameters);
+
+            // SINGLE <RESOURCE> NOT FOUND
+            urlParameters = @"unknown/23";
+            await REST.Get(urlParameters);
+
+            // CREATE
+            urlParameters = @"users";
+            await REST.Create(urlParameters);
+        }
+    }
+}
